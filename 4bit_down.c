e75d6f05951f9b79a53779e84b7ad3c26048a75f
M MELVIN MOHAN <melvinmohan1256@gmail.com>
2024-07-19 14:35:36 +0530

    Add files via upload

---
+++ 4bit_down.c
@@ -0,0 +1,98 @@
+#include <lpc214x.h>
+void delay(unsigned int count);
+int main() {
+
+IODIR0 |= (1 << 1);
+IODIR0 |= (1 << 3);
+IODIR0 |= (1 << 4);
+IODIR0 |= (1 << 5);
+  // Set P0.10 as output
+
+while (1){
+	IOCLR0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+  delay(400000);
+  IOCLR0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+  delay(400000);
+	IOCLR0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOCLR0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOCLR0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOCLR0 |= (1 << 4);
+	delay(400000);
+	IOSET0 |= (1 << 5);
+	IOSET0 |= (1 << 1);
+	IOSET0 |= (1 << 3);
+	IOSET0 |= (1 << 4);
+	delay(400000);
+      }
+}
+void delay(unsigned int count)
+{
+	unsigned int i;
+	for(i=0;i<count;i++);
+}
