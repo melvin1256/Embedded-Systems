23fda5ead8bb9caa07f7b652fcb077f0d094e9c4
M MELVIN MOHAN <melvinmohan1256@gmail.com>
2024-07-22 14:36:46 +0530

    Add files via upload

--- SEVEN.C
+++ SEVEN.C
@@ -12,46 +12,38 @@ IODIR0|=(0x01<<6);
 	IODIR0|=(0x01<<9);//set direction as output
 if((0x01<<16)&IOPIN1){
   // Set P0.10 as output
-
-	IOSET0 |= (1 << 4);
+	//1
+  IOSET0 |= (1 << 4);
 	IOSET0 |= (1 << 5);
-	IOCLR0 |= (1 << 3);
-	IOCLR0 |= (1 << 6);
-	IOCLR0 |= (1 << 7);
-	IOCLR0 |= (1 << 8);
-	IOCLR0 |= (1 << 9);
-
-	
 	delay(400000);
+	//2
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
 	IOSET0 |= (1 << 9);
 	IOSET0 |= (1 << 7);
 	IOSET0 |= (1 << 6);
-	IOCLR0 |= (1 << 5);
 	delay(400000);
+	//3
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
 	IOSET0 |= (1 << 9);
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 6);
-	IOCLR0 |= (1 << 7);
 	delay(400000);
+	//4
 	IOSET0 |= (1 << 8);
 	IOSET0 |= (1 << 9);
 	IOSET0 |= (1 << 5);
-	IOCLR0 |= (1 << 6);
-	IOCLR0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
-	
 	delay(400000);
+	//5
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 8);
 	IOSET0 |= (1 << 9);
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 6);
-	IOCLR0 |= (1 << 4);
-	delay(400000);
+  delay(400000);
+	//6
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 8);
 	IOSET0 |= (1 << 7);
@@ -59,14 +51,12 @@ if((0x01<<16)&IOPIN1){
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 9);
   delay(400000);
+	//7
   IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
-	IOCLR0 |= (1 << 6);
-	IOCLR0 |= (1 << 7);
-	IOCLR0 |= (1 << 8);
-	IOCLR0 |= (1 << 9);
-  delay(400000);
+	delay(400000);
+	//8
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 9);
 	IOSET0 |= (1 << 3);
@@ -75,21 +65,21 @@ if((0x01<<16)&IOPIN1){
 	IOSET0 |= (1 << 7);
 	IOSET0 |= (1 << 8);
 	delay(400000);
+	//9
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 8);
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
 	IOSET0 |= (1 << 9);
-	IOCLR0 |= (1 << 6);
-	IOCLR0 |= (1 << 7);
+	IOSET0 |= (1 << 6);
 	delay(400000);
+	//0
 	IOSET0 |= (1 << 5);
 	IOSET0 |= (1 << 6);
 	IOSET0 |= (1 << 3);
 	IOSET0 |= (1 << 4);
 	IOSET0 |= (1 << 7);
 	IOSET0 |= (1 << 8);
-	IOCLR0 |= (1 << 9);
 	delay(400000);
 	
 }
@@ -99,4 +89,12 @@ void delay(unsigned int count)
 {
 	unsigned int i;
 	for(i=0;i<count;i++);
+		IOCLR0 |= (1 << 9);
+		IOCLR0 |= (1 << 8);
+		IOCLR0 |= (1 << 7);
+		IOCLR0 |= (1 << 6);
+		IOCLR0 |= (1 << 5);
+		IOCLR0 |= (1 << 4);
+		IOCLR0 |= (1 << 3);
+
 }
